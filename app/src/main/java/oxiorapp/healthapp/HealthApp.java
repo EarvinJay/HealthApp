242ed91b1adcbfaabf56b49d4aea0681d50cd8b8
EarvinJay <earvinjaybermudo21@gmail.com>
2016-01-15 09:37:46 +0800

    LoginActivity implemented

---
+++ app/src/main/java/oxiorapp/healthapp/HealthApp.java
@@ -0,0 +1,36 @@
+package oxiorapp.healthapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class HealthApp extends AppCompatActivity {
+EditText username,password;
+    Button logins;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_app);
+
+        username=(EditText) findViewById(R.id.txtusername);
+        password=(EditText) findViewById(R.id.txtpassword);
+        logins=(Button) findViewById(R.id.login);
+
+
+
+    }
+
+    public void login(View view)
+    {
+
+            Intent intent = new Intent(this, home.class);
+            startActivity(intent);
+
+
+    }
+
+}
