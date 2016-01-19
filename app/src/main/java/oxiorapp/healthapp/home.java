8ff632483f770d2d8aad1bbf8671d198e9050722
EarvinJay <earvinjaybermudo21@gmail.com>
2016-01-19 11:59:05 +0800

    Latest Commit (UserInfo)

--- app/src/main/java/oxiorapp/healthapp/home.java
+++ app/src/main/java/oxiorapp/healthapp/home.java
@@ -1,7 +1,9 @@
 package oxiorapp.healthapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class home extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void toUserInfo(View v)
+    {
+        Intent intent=new Intent(this,UserInfo.class);
+        startActivity(intent);
     }
 }
