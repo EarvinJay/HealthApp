5f0e1624442a59693428062360d8921f88196563
EarvinJay <earvinjaybermudo21@gmail.com>
2016-01-20 11:43:45 +0800

    Latest Commit(findviewbyids , intent to the next activity)

--- app/src/main/java/oxiorapp/healthapp/UserInfo.java
+++ app/src/main/java/oxiorapp/healthapp/UserInfo.java
@@ -1,13 +1,25 @@
 package oxiorapp.healthapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class UserInfo extends AppCompatActivity {
-
+    String dates,weights,heights,bloodtypes,allergy,foodprefs,weightobjs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        Intent intent=getIntent();
+        dates=intent.getStringExtra("data_date");
+        weights=intent.getStringExtra("data_weight");
+        heights=intent.getStringExtra("data_height");
+        bloodtypes=intent.getStringExtra("data_bloodtype");
+        allergy=intent.getStringExtra("data_allergies");
+        foodprefs=intent.getStringExtra("data_foodpref");
+        weightobjs=intent.getStringExtra("data_weightobj");
+
+
     }
 }
--- app/src/main/java/oxiorapp/healthapp/home.java
+++ app/src/main/java/oxiorapp/healthapp/home.java
@@ -4,18 +4,46 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class home extends AppCompatActivity {
 
+    private EditText date,weight,height,bloodtype,allergies,foodpref,weightobj;
+    String dates,weights,heights,bloodtypes,allergy,foodprefs,weightobjs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        date=(EditText) findViewById(R.id.txtdate);
+        dates=date.getText().toString();
+        weight=(EditText) findViewById(R.id.txtweight);
+        weights=weight.getText().toString();
+        height=(EditText) findViewById(R.id.txtheight);
+         heights=height.getText().toString();
+        bloodtype=(EditText) findViewById(R.id.txtbloodtype);
+         bloodtypes=bloodtype.getText().toString();
+        allergies=(EditText) findViewById(R.id.txtallergies);
+         allergy=allergies.getText().toString();
+        foodpref=(EditText) findViewById(R.id.txtfoodpref);
+         foodprefs=foodpref.getText().toString();
+        weightobj=(EditText) findViewById(R.id.txtweightobj);
+       weightobjs=weightobj.getText().toString();
+
+
     }
 
     public void toUserInfo(View v)
     {
         Intent intent=new Intent(this,UserInfo.class);
+        intent.putExtra("data_date",dates);
+        intent.putExtra("data_weight",weights);
+        intent.putExtra("data_height",heights);
+        intent.putExtra("data_bloodtype",bloodtypes);
+        intent.putExtra("data_allergies",allergy);
+        intent.putExtra("data_foodpref",foodprefs);
+        intent.putExtra("data_weightobj",weightobjs);
         startActivity(intent);
     }
 }
